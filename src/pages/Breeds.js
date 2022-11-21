612c4317a42dee2a4d3a69dc13f94c8c895e80b6
Shiv Matta <shiv.matta@gmail.com>
2022-11-20 17:50:17 -0800

    removed form due to errors

--- src/pages/Breeds.js
+++ src/pages/Breeds.js
@@ -3,30 +3,13 @@ import React, {useState, useEffect} from "react";
 
 function Breeds ({breeds}){
 
-    const [newDog, setNewDog] = useState({breed: ''})
-    const [newBreeds, setNewBreeds] = useState({breeds})
-    
-    function handleNewDog(event){
-        setNewDog({[event.target.id]: event.target.value})
-    }
 
-    function handleSubmit (e){
-        e.preventDefault();
-        const newBreeds = breeds.append({newDog})
-        
-        setNewBreeds(newBreeds)
-    }
 
 
        
 return (
     <div className="Breeds">
         <h1>Master List of Breeds</h1>
-        <h2>Don't see your dog's breed? Add it in!</h2>
-        <form> 
-            <button onSubmit={handleSubmit}>Add a Dog</button>
-            <input type = 'submit' id = 'newDog' placeholder="Add a dog!" value = {newDog} onChange = {handleNewDog}> </input>
-        </form>
         <ul>
             {breeds.map((breed) => {
                  return (
